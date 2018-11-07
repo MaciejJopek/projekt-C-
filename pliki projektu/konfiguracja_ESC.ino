ce7153f69a33e7ab473be4281eec5e0594cd7da6
Unknown <297615@uwr.edu.pl>
2018-11-08 00:33:03 +0100

    Aktualizacja sterownika ESC
    Dodano obsługę pozostałych 3 silników.

--- pliki projektu/konfiguracja_ESC.ino
+++ pliki projektu/konfiguracja_ESC.ino
@@ -6,10 +6,15 @@
 #define MIN_SIGNAL 1000
 
 #define MOTOR_PIN 9
-
+#define MOTOR_PIN2 6
+#define MOTOR_PIN3 5
+#define MOTOR_PIN4 3
 int value = 1000;
 
 Servo silnik;
+Servo silnik2;
+Servo silnik3;
+Servo silnik4;
 
 
 void setup() {
@@ -19,12 +24,18 @@ void setup() {
   Serial.println("Przed podłączniem baterii należy skonfigurować ESC.");
 
   silnik.attach(MOTOR_PIN);
+  silnik2.attach(MOTOR_PIN2);
+  silnik3.attach(MOTOR_PIN3);
+  silnik4.attach(MOTOR_PIN4);
 
   Serial.print("Ustawianie maksymalnej wartości dla silnika: (");Serial.print(MAX_SIGNAL);Serial.print("\n");
 
   Serial.println("Teraz można podłaczyć zasilanie, a następnie naciśnij dowolny klawisz");Serial.println("\n");
 
   silnik.writeMicroseconds(MAX_SIGNAL);
+  silnik2.writeMicroseconds(MAX_SIGNAL);
+  silnik3.writeMicroseconds(MAX_SIGNAL);
+  silnik4.writeMicroseconds(MAX_SIGNAL);
 
 
   while (!Serial.available());
@@ -34,6 +45,9 @@ void setup() {
   Serial.print("Ustawianie minimalnej wartości dla silnika: (");Serial.print(MIN_SIGNAL);Serial.print("\n");
 
   silnik.writeMicroseconds(MIN_SIGNAL);
+  silnik2.writeMicroseconds(MIN_SIGNAL);
+  silnik3.writeMicroseconds(MIN_SIGNAL);
+  silnik4.writeMicroseconds(MIN_SIGNAL);
 
   Serial.println("ESC zostało prawidłowo skonfigurowane");Serial.print("\n");
 
@@ -56,6 +70,9 @@ void loop() {
     if (value > 999)
     {
       silnik.writeMicroseconds(value);
+	  silnik2.writeMicroseconds(value);
+	  silnik3.writeMicroseconds(value);
+	  silnik4.writeMicroseconds(value);
 
       float predkosc = (value-1000)/10;
 
